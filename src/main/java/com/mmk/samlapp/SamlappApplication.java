b17df31da350d09edef1a4dcde3768447990bfab
jon <manojam79@gmail.com>
2019-07-24 22:51:55 +1000

    saml sso spring boot

---
+++ src/main/java/com/mmk/samlapp/SamlappApplication.java
@@ -0,0 +1,21 @@
+package com.mmk.samlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class SamlappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlappApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+}
